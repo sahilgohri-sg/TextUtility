1e50c29d51f210b9435349d96ab604a82577e7ee
Sahil Gohri <sahilgohri.sg@gmail.com>
2022-07-07 02:26:20 +0530

    Updates

---
+++ precache-manifest.6df0a611be5df7e6587897d18dfed3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a2783ddcb9389f591d",
+    "url": "/TextUtility/static/css/main.c7e1d057.chunk.css"
+  },
+  {
+    "revision": "16a2783ddcb9389f591d",
+    "url": "/TextUtility/static/js/main.16a2783d.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtility/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "92d9ee8a934a4d14bee9",
+    "url": "/TextUtility/static/js/2.92d9ee8a.chunk.js"
+  },
+  {
+    "revision": "c102aa0d82f7e7d55ee0",
+    "url": "/TextUtility/static/js/runtime~main.c102aa0d.js"
+  },
+  {
+    "revision": "464c0df46844e47164544e66724f2dfe",
+    "url": "/TextUtility/index.html"
+  }
+];
